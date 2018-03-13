f3d64e73cfe3acec9f259e9b33344668f55b74d7
unblee <unblee@users.noreply.github.com>
2018-03-13 18:27:12 +0900

    add main struct

--- pick.go
+++ pick.go
@@ -1,4 +1,24 @@
 package pick
 
+import (
+	"errors"
+	"io"
+	"io/ioutil"
+	"strings"
+)
+
 type Pick struct {
+	stdin          io.Reader
+	stdout, stderr io.Writer
+}
+
+func New(stdin io.Reader, stdout, stderr io.Writer) *Pick {
+	return &Pick{
+		stdin:  stdin,
+		stdout: stdout,
+		stderr: stderr,
+	}
+}
+
+func (p *Pick) Exec() {
 }
